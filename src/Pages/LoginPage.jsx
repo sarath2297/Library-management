1418f5fdd014e724fc157ba008f46ee52bb28170
Nevin <nevinjohn63@gmail.com>
2024-04-27 11:20:01 +0530

    added Pages for different user

---
+++ src/Pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+const LoginPage = () => {
+    
+  const[login,setLogin] = useState();
+
+  return (
+    <div className='w-full '>
+      
+    </div>
+  )
+}
+
+export default LoginPage

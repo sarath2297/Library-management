d1f23590601397cee4d131acccbaf6a55ef1b5f8
anjana291 <anjanababum123@gmail.com>
2024-04-27 12:08:31 +0530

    check branch

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
 import React from 'react'
 
 function UserPage() {
   return (
-    <>
-    </>
+    <div>UserPage</div>
   )
 }
 
 export default UserPage
-=======
-const UserPage = () => {
-  return (
-    <div>
-      <p>User Page</p>
-    </div>
-  )
-}
 
-export default UserPage
->>>>>>> a741e9968508cac9355e133c73854b12b39fbf99

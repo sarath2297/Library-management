3ad48ce4ccdf1d04b71692b10081f53cbff66579
Nevin <nevinjohn63@gmail.com>
2024-04-27 11:58:38 +0530

    added files

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -4,8 +4,11 @@ const LoginPage = () => {
     
 
   return (
-    <div className={`${LoginPageStyles.loginPage} w-full vh-100  loginPage`}>
-      
+    <div className={`${LoginPageStyles.loginPage} w-full vh-100  loginPage d-flex flex-column align-items-center`}>
+          <div className="w-50 pt-4">
+            img
+            <h1 className='text-info text-center'>Login Page</h1>
+          </div>
     </div>
   )
 }

87efeb19b5e910a0c98d2ba5da3b2f18f6355f96
jijopattasseriyi <sijomadakampoil123@gmail,com>
2024-04-29 20:50:07 +0530

    list of books page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import LoginPage from './Pages/LoginPage'
 import UserPage from './Pages/UserPage'
 import AdminPage from './Pages/AdminPage'
+import List_of_Books from './Pages/List_of_Books'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<LoginPage/>}/>
         <Route path='/user' element={<UserPage/>}/>
         <Route path='/admin' element={<AdminPage/>}/>
+        <Route path='/list' element={<List_of_Books/>}/>
 
       </Routes>
     </>
